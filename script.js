46a6d79ee1cce06e7df96b7a82a7d489d9dbbf5b
YuraVasylyshyn <yura.vas535@gmail.com>
2020-08-07 23:52:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+    $(document).ready(function() {
+        $('.inst').click(function(event) {
+          $('.alert-inst').css("display","block");
+            $('.content').css("display","none")
+        });
+      });
+
+      $(document).ready(function() {
+        $('.ssss').click(function(event) {
+          $('.alert-face').css("display","block");
+            $('.content').css("display","none")
+        });
+      });
+
+        $(document).ready(function() {
+        $('.fa-times').click(function(event) {
+            $('.alert').css("display","none");
+              $('.content').css("display","block")
+        });
+      });
+
+
